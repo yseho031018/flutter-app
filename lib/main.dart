673b6223df7cebc3f2df3d9ed0360bf7bbcaa4b4
seho <sehoho1018@gmail.com>
2025-09-30 12:11:32 +0900

    0930

--- lib/main.dart
+++ lib/main.dart
@@ -32,17 +32,84 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: createBody1(),
-    );
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: Text(widget.title),
+        ),
+        body: Center(
+          child: createBody3(),
+        ));
   }
 }
 
+// Container -> Text
 Widget createBody1() {
+  // 위젯 트리
   return Container(
+    padding: const EdgeInsets.only(
+      left: 20,
+      right: 20,
+    ),
+    width: 200,
+    height: 100,
+    alignment: Alignment.center,
     color: Colors.red,
+    child: const Text('Container'),
+  );
+}
+
+Widget createBody2() {
+  return Container(
+      padding: const EdgeInsets.only(
+        left: 20,
+        right: 20,
+      ),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            const Color.fromARGB(255, 255, 59, 98).withOpacity(0.7),
+            const Color.fromARGB(255, 255, 59, 98)
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(255, 255, 59, 98).withOpacity(0.5),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: const Offset(0, 3),
+          ),
+        ],
+      ),
+      width: 200,
+      height: 150,
+      child: const Center(
+          child: Text(
+            'Container',
+            style: TextStyle(color: Colors.white),
+      ),
+    ),
+  );
+}
+
+Widget createBody3(){
+  return Container(
+    color: Colors.blue,
+    height: 100,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: List.generate(
+        5,
+            (index) => Container(
+          width: 40,
+          height: 40,
+          color: Colors.red,
+          margin: const EdgeInsets.all(5),
+        ),
+      ),
+    ),
   );
 }
